7c7ed2c19a16c45c230e80e0ee88f118c041c40a
Carol_Inglethe <carolinainglethe@gmail.com>
2022-02-25 09:36:28 -0300

    instala depencia router e inicia rota login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <p> Pagina inicial</p>
+     <BrowserRouter>
+       <Routes>
+         <Route path="/login" element={ <Login/> }/>
+         <Route path="/"> <Navigate to="/login"/> </Route>
+       </Routes>
+     </BrowserRouter>
     </div>
   );
 }
