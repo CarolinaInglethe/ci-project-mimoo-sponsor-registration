88a3b650eb60c3670b7355829f39a38128a4062b
Carol_Inglethe <carolinainglethe@gmail.com>
2022-02-28 16:23:12 -0300

    Sponsers - organiza pagina organizadores e navbar , CSS

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoMimoo from '../images/logo-mimoo.png';
+import logoGrafico from '../images/logo-grafico.png'
+import logoSorriso from '../images/logo-sorriso.png'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className="nav-bar-container">
     <img src={logoMimoo} alt="logo" />
-      NavBar
+    <Link to="/dashboard"> 
+      <img src={logoGrafico} alt="logo grafico" width="25px" />
+      Dashboard 
+    </Link>
+    <Link to="/sponsors">
+        <img src={logoSorriso} alt="logo sorriso" width="25px"/>
+        Patrocinadores
+    </Link>
     </div>
     
   )
--- src/pages/ Sponsors.tsx
+++ src/pages/ Sponsors.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import logoSorriso from '../images/logo-sorriso.png'
 
 const Sponsors = () => {
   return (
-    <div>
+    <div className="sponsors-container">
       <NavBar/>
-      <h3>Pagina Patrocinadores</h3>
+
+      <div>
+      <h2> 
+        <img src={logoSorriso} alt="logo Sorriso" width="40px"/> 
+        Patrocinadores
+      </h2>
+      <p>Patrocinadores</p>
+      <h3>Tabela</h3>
+      </div>
+
     </div>
   )
 }
