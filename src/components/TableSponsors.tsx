69359cfe02f9722ab0bad5477b53902e25b0ce10
Carol_Inglethe <carolinainglethe@gmail.com>
2022-02-28 17:20:11 -0300

    Sponsors - estrutura basica Tabela , componente TableSponsors

--- src/components/TableSponsors.tsx
+++ src/components/TableSponsors.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
 const TableSponsors = () => {
+  const columns = [ 
+    'Patrocinador', 'Situação', 'Homologada', 'Marcas Cadastradas', 
+    'Marcas Ativas', 'Última ação', 'Status',
+  ]
   return (
     <div>
       <h3>
-        Table Sponsors
+        Patrocinadores
       </h3>
+
+      <table style={ { border:"1px" } }>
+            <tr>
+              {
+                columns.map((colum, index) => (
+                  <td key={index}>{colum}</td>
+                ))
+              }
+              <td>bb</td>
+              <td>bb</td>
+            </tr>
+            <tr> 
+              <td>hello</td>
+              <td>hello</td>
+            </tr>
+        </table>
     </div>
   )
 }
