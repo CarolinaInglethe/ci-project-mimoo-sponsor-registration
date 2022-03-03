5c7b1f6c1e7ab1b6e3862c2f6eebf231242f6c22
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-02 22:22:39 -0300

    Pagina Cadastra Patrocinador - , cria componente formulario, insere css

---
+++ src/components/FormCreateSponsor.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const FormCreateSponsor = () => {
+ 
+  return (
+    <div className="form-create-sponsor-container">
+      <h2>form Cria sponsor</h2>
+    </div>
+  )
+}
+
+export default FormCreateSponsor;
--- src/pages/CreateSponsor.tsx
+++ src/pages/CreateSponsor.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import FormCreateSponsor from '../components/FormCreateSponsor';
+import NavBar from '../components/NavBar';
+import logoSorriso from '../images/logo-sorriso.png'
 
 const CreateSponsor = () => {
   return (
-    <div>
-      <h1>Page Create Sponsor</h1>
+    <div className="create-sponsor-container">
+      <NavBar />
+
+      <div className="create-sponsor-container-2">
+      <h2>
+      <img src={logoSorriso} alt="logo sorriso" width="25px"/>
+      Cadastro Patrocinador
+      </h2>
+      <p>Patrocinadores {'>'} Cadastro Patrocinador</p>
+
+      <FormCreateSponsor />
+      </div>
+      
     </div>
   )
 }
