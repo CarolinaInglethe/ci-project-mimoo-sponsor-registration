a048b2dddc8b8181a91e0c83b6c2bfbb046cd2d7
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-04 10:28:18 -0300

    Estrutura componente form de cadastro 3

--- src/components/FormCreateSponsor-3.tsx
+++ src/components/FormCreateSponsor-3.tsx
@@ -9,45 +9,16 @@ const FormCreateSponsor3 = () => {
     <div className="form-create-sponsor-1">
       <NumberPagesForm />
       <p>Form 3</p>
-      {/* <form>
+      <form>
         <div>
-        <h3>Informações gerais</h3>
-        <input type="image" alt="Clique para adicionar uma imagem" />
+        <h3>Marcas</h3>
+        <button>Vincular marcas</button>
+        </div>
+        
         <input type="text" placeholder="Empresa" />
         <input type="text" placeholder="CNPJ" />
         <input type="text" placeholder="Saldo de pontos" />
-        <label htmlFor="prospect">
-          Prospect ?
-          <input type="checkbox" name="prospect" id="prospect" />
-        </label>
-        <label htmlFor="homologada">
-          Homologada ?
-          <input type="checkbox" name="homologada" id="homologada" />
-        </label>
-        </div>
-        <div>
-          <h3>Selecione o tipo de conta</h3>
-            <input type="radio" id="industria" name="tipoconta" value="Indústria"/>
-            <label htmlFor="industria">Indústria</label>
-
-            <input type="radio" id="varejo" name="tipoconta" value="Varejo"/>
-            <label htmlFor="varejo">Varejo</label>
-
-            <input type="radio" id="shopping" name="tipoconta" value="Shopping"/>
-            <label htmlFor="shopping">Shopping</label>
-
-            <input type="radio" id="agencia" name="tipoconta" value="Agência"/>
-            <label htmlFor="agencia">Agência</label>
-        </div>
-        <div>
-            <h3>Selecione o tipo de Plano</h3>
-            <input type="radio" id="planoprepago" name="tipoplano" value="Pré-pago"/>
-            <label htmlFor="planoprepago">Pré-pago</label>
-
-            <input type="radio" id="planopospago" name="tipoplano" value="Pós-pago"/>
-            <label htmlFor="planopospago">Pós-pago</label>
-        </div>
-      </form> */}
+      </form>
 
       <ButtonsNextBackForm />
     </div>
